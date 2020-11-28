5ba9c1176286339efe51a45ce0c20424d2618c7a
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-11-28 10:18:13 -0500

    Creating tables in process

---
+++ script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Casboni;
+
+CREATE TABLE Vehiculo (
+    placa varchar(6),
+    nVim varchar(17),
+    nMotor varchar(14),
+    PRIMARY KEY (placa)
+)
+
+CREATE TABLE Cliente (
+
+)
